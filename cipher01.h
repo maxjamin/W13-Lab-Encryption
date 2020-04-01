73c75878ff0ef813495a7884b58c1dde9933b87e
maxjamin <maxjamin@gmail.com>
2020-04-01 11:28:16 -0600

    changing 1

--- cipher01.h
+++ cipher01.h
@@ -23,7 +23,7 @@ public:
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
-      return std::string("citation");
+      return std::string("citation: https://www.researchgate.net/publication/311424500_An_Overview_of_the_RC4_Algorithm, https://stepuptocrypt.blogspot.com/2019/02/symmetric-cryptography-rc4-algorithm.html\n ");
    }
    
    /**********************************************************
@@ -36,7 +36,21 @@ public:
 
       // TODO: please format your pseudocode
       // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
+      str =  "Two parts of the algorithm\n",
+              "Key-scheduling algorithm\n",
+                "for i from 0 to 255\n"
+                  "s[i] := 1\n",
+              "endfor\n",
+              "\n",
+              "i, j :=0\n",
+              "while(generatingOutput:\n",
+                "i := (i + 1) mod 256\n",
+                "j := (j + S[i]) mod 256\n",
+                "swap values of S[i] and S[j]\n",
+                "K := S[(S[i] + S[j]) mod 256]\n",
+                "output K\n",
+              "endwhile\n"
+              ;
 
       // The decrypt pseudocode
       str += "insert the decryption pseudocode\n";
@@ -64,6 +78,23 @@ public:
 
    }
 
+   int pseudoRandomGeneration(int *streamInput, 
+                              int & j,
+                              int & i)
+   {
+
+      std::cout << "i is " << i << " j is " << j << "\n";
+      while(1)
+      {
+        i = (i+1) % 256;
+        j = (j + streamInput[i]) % 256;
+        std::swap(streamInput[i], streamInput[j]);
+
+        int t = (streamInput[i] + streamInput[j]) % 256;
+        return streamInput[t];
+      }
+   }
+
    /**********************************************************
     * ENCRYPT
     * TODO: ADD description
@@ -71,13 +102,31 @@ public:
    virtual std::string encrypt(const std::string & plainText,
                                const std::string & password)
    {
-      std::string cipherText = plainText;
+      std::string cipherText;
       int initalVector[256];
 
       keyRandomizeAlgorithm(initalVector, password);
 
-      for (int h=0; h < 255; h++)
-          std::cout << initalVector[h] << "-";
+      int i = 0;
+      int j = 0;
+      for(std::string::size_type h=0; h < plainText.size(); h++)
+      {
+        int k = pseudoRandomGeneration(initalVector, j, i);
+
+        //convert plaintext to int
+        int plain_as_int = (int)plainText[h];
+        //std::cout << "plain_as_int " << plain_as_int << "\n";
+
+        //XOR plaintext and k from keystream
+        int output = k^plain_as_int;
+
+        std::cout << "plainText: " << plainText[h] << " - k being " <<
+        k << " Converted to " << output <<"\n";
+
+        //add to cipherText
+        cipherText.push_back((char)output);
+        std::cout << "cipherText is " << cipherText[h] << " - ";
+      }
 
       return cipherText;
    }
