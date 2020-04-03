bd12f78f6f2bf3312e91053c2316fb8146d5f44b
Tyler <sta14031@byui.edu>
2020-04-03 16:53:39 -0600

    More work on cipher02

--- cipher02.h
+++ cipher02.h
@@ -14,8 +14,8 @@ class Cipher02 : public Cipher
 public:
    virtual std::string getPseudoAuth()  { return "Tyler Starr"; }
    virtual std::string getCipherName()  { return "TEA (Tiny Encryption Algorithm)"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
-   virtual std::string getDecryptAuth() { return "decrypt author"; }
+   virtual std::string getEncryptAuth() { return "Tyler Starr"; }
+   virtual std::string getDecryptAuth() { return "Tyler Starr"; }
 
    /***********************************************************
     * GET CIPHER CITATION
@@ -23,7 +23,7 @@ public:
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
-      return std::string("citation");
+      return std::string("http://citeseer.ist.psu.edu/viewdoc/download;jsessionid=C08E8409ADF484095568965A1EBF3E5E?doi=10.1.1.45.281&rep=rep1&type=pdf");
    }
    
    /**********************************************************
@@ -52,8 +52,44 @@ public:
                                const std::string & password)
    {
       std::string cipherText = plainText;
-      // TODO - Add your code here
+
+      // This algorithm can cause stack smashing if a
+      // block is less than 8 bytes in length
+      while (cipherText.size() % 8 != 0)
+         cipherText += ' ';
+
+      long* v = (long*)(cipherText.c_str());
+      long* k = (long*)(password.c_str());
+
+      // Each block is 8 bytes in size.
+      int numIterations = cipherText.size() / 8 + 1;
+      while (numIterations --> 0)
+         encrypt_8(v++, k);
+
       return cipherText;
+   }
+
+   /**********************************************************
+    * ENCRYPT_8
+    * Encrypts 8 bytes of v using k as the key
+    **********************************************************/
+   void encrypt_8(long* v, long* k)
+   {
+      unsigned long y = v[0];
+      unsigned long z = v[1];
+      unsigned long sum = 0;
+      unsigned long delta = 0x9e3779b9;
+      unsigned long n = 32;
+
+      while (n --> 0)
+      {
+         sum += delta;
+         y += (z << 4) + k[0] ^ z + sum ^ (z >> 5) + k[1];
+         z += (y << 4) + k[2] ^ y + sum ^ (y >> 5) + k[3];
+      }
+      
+      v[0] = y;
+      v[1] = z;
    }
 
    /**********************************************************
@@ -64,8 +100,41 @@ public:
                                const std::string & password)
    {
       std::string plainText = cipherText;
-      // TODO - Add your code here
+
+      long* v = (long*)(plainText.c_str());
+      long* k = (long*)(password.c_str());
+
+      // Apply the decryption to the same 8-byte size blocks
+      int numIterations = plainText.size() / 8 + 1;
+      while (numIterations --> 0)
+         decrypt_8(v++, k);
+
       return plainText;
+   }
+
+   /**********************************************************
+    * DECRYPT_8
+    * Decrypts 8 bytes of v using k as the key
+    **********************************************************/
+   void decrypt_8(long* v, long* k)
+   {
+      unsigned long n = 32;
+      unsigned long sum;
+      unsigned long y = v[0];
+      unsigned long z = v[1];
+      unsigned long delta = 0x9e3779b9;
+
+      sum = delta << 5;
+
+      while (n --> 0)
+      {
+         z -= (y << 4) + k[2] ^ y + sum ^ (y >> 5) + k[3];
+         y -= (z << 4) + k[0] ^ z + sum ^ (z >> 5) + k[1];
+         sum -= delta;
+      }
+
+      v[0] = y;
+      v[1] = z;
    }
 };
 
